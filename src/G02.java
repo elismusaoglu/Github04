97fe3ebd3413e3358fc8a5647432f5a785812c10
elismusaoglu <elis.musaoglu@gmail.com>
2023-05-28 16:10:04 +0100

    4.gun

---
+++ src/G02.java
@@ -0,0 +1,24 @@
+public class G02 {
+
+    /*
+    Git-Github da projelerimiz uzerinde calisirken temelde 3 adimda islem yapiyoruz
+
+        1-Projelerimizi/Dosyalarimizi git ile iliskilendiriyoruz
+            git init
+
+        2- projemizde/dosyamizda bulunanlari versiyon olusturuyorduk
+
+           git add
+           git commit -m"versiyonadi"
+
+         3- github'a yollama/entegrasyonu
+
+            git remote add origin repoUrlAdresi (Ilk defa yapiyorsak)
+            already origin
+            git remote set-url repoUrlAdresi
+
+            git push
+     */
+
+
+}
